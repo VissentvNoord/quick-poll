75984e5ac95dec2216291cdcbd23fe313c1b0113
vincentvnoord <vannoord.vincent@gmail.com>
2024-02-03 20:20:56 +0100

    CORS

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,25 @@ import (
 	"net/http"
 	"quick-poll/internal/poll"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
 
+	// Register routes
 	r.HandleFunc("/new", poll.CreatePoll).Methods("POST")
 	r.HandleFunc("/{id}", poll.GetPoll).Methods("GET")
 	r.HandleFunc("/{id}/submit", poll.UserSubmission).Methods("POST")
 
-	http.Handle("/", r)
+	headersOk := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT", "OPTIONS"})
+
+	corsRouter := handlers.CORS(headersOk, originsOk, methodsOk)
+
+	http.Handle("/", corsRouter(r))
 
 	port := ":8080"
 	println("Server listening on port", port)
